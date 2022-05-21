53f7a76d032afdc73b7039293b81d00f2f825a91
Lee TaeWoo <syr@meu.works>
2022-05-21 21:13:53 +0900

    change impls of router/getBookImageList

--- src/route/getBookImageList.ts
+++ src/route/getBookImageList.ts
@@ -5,18 +5,27 @@ import { catcher } from "./lib/catcher";
 
 const log = getLogger("route/getBookImageList");
 
-export const getBookImageList = router.get(
+export const getBookImageListUrl = router.get(
     "/books/:bookId/images",
     async (ctx) => {
         const { bookId } = ctx.params;
 
         try {
-            const imageList = await usecase.getBookImageList.execute(
+            /* const imageList = await usecase.getBookImageList.execute(
                 parseInt(bookId, 10),
             );
-
+            
             ctx.status = 200;
             ctx.body = imageList;
+            */
+
+            const imageListUrl =
+                await usecase.getBookImageUrl.execute(
+                    parseInt(bookId, 10),
+                    "image_list",
+                );
+
+            ctx.redirect(imageListUrl);
         } catch (error) {
             log.error(error);
             catcher(error, ctx);
