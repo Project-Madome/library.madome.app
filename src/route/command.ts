3538128c3a5f90fa0da3c81bb611a43a1ed27f99
Lee TaeWoo <syr@meu.works>
2022-05-25 23:12:00 +0900

    change args of command

--- src/route/command.ts
+++ src/route/command.ts
@@ -7,7 +7,7 @@ import { getLogger } from "../logger";
 import { isNullOrUndefined } from "../type/guard";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const log = getLogger("route/addBook");
+const log = getLogger("route/command");
 
 const hasBook = (id: number): Promise<boolean> => {
     return getRepository(entity.Book)
@@ -16,7 +16,6 @@ const hasBook = (id: number): Promise<boolean> => {
 };
 
 const hasBookTag = (kind: string, name: string): Promise<boolean> => {
-    // TODO: validate kind
     if (!dto.BookTagKind.includes(kind, "snake")) {
         return Promise.resolve(false);
     }
@@ -35,19 +34,26 @@ router.post("/command", async (ctx) => {
     switch (kind) {
         case "has_book":
             {
+                log.debug("has_book");
+
                 const { book_id: bookId } = ctx.request.body;
 
+                ctx.status = 200;
                 ctx.body = { has: await hasBook(bookId) };
             }
             break;
 
         case "has_book_tag":
             {
-                const { tag_kind: tagKind, tag_name: tagName } =
-                    ctx.request.body;
+                log.debug("has_book_tag");
 
+                const { book_tag: bookTag } = ctx.request.body;
+
+                const [kind, name] = bookTag;
+
+                ctx.status = 200;
                 ctx.body = {
-                    has: await hasBookTag(tagKind, tagName),
+                    has: await hasBookTag(kind, name),
                 };
             }
             break;
